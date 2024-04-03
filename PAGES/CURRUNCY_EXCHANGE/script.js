de6b34edf69a3e0cb409db9bd8e9f78dc41ff4a2
KISHAN YADAV <108346805+KISHAN-Y@users.noreply.github.com>
2024-04-03 22:49:58 +0530

    script.js

--- PAGES/CURRUNCY_EXCHANGE/script.js
+++ PAGES/CURRUNCY_EXCHANGE/script.js
@@ -1,66 +1,78 @@
 const select = document.querySelectorAll(".currency");
-const btn = document.getElementById("btn"); 
+const btn = document.getElementById("btn");
 const num = document.getElementById("num");
 const ans = document.getElementById("ans");
 
 fetch("https://api.frankfurter.app/currencies")
-  .then((data) => data.json())
-  .then((data) => {
-    display(data);
-  });
+    .then((data) => data.json())
+    .then((data) => {
+        display(data);
+    });
 
 function display(data) {
-  const entries = Object.entries(data);
-  for (var i = 0; i < entries.length; i++) {
-    select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
-    select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
-  }
+    const entries = Object.entries(data);
+    for (var i = 0; i < entries.length; i++) {
+        select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+        select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+    }
 }
 
 // Real-time conversion (on input change)
 num.addEventListener('input', () => {
-  convertFromInput();
+    convertFromInput();
 });
 
 // Conversion on button click
 btn.addEventListener("click", () => {
-  convertFromInput(); 
+    convertFromInput();
 });
 
 function convertFromInput() {
-  let currency1 = select[0].value;
-  let currency2 = select[1].value;
-  let value = num.value;
+    let currency1 = select[0].value;
+    let currency2 = select[1].value;
+    let value = num.value;
 
-  if (currency1 === currency2) { 
-    if (confirm("You've selected the same currency. Change one of them?")) {
+    if (currency1 === currency2) {
+        if (confirm("You've selected the same currency. Change one of them?")) {} else {
+            ans.value = "";
+        }
+    } else if (value !== "") {
+        convert(currency1, currency2, value);
     } else {
-      ans.value = "";
+        ans.value = "";
     }
-  } else if (value !== "") { 
-    convert(currency1, currency2, value);
-  } else {
-    ans.value = ""; 
-  }
 }
 
 function setcountryflag1(code) {
-  var flag = code.value.substr(0, code.value.length - 1).toLowerCase();
-  document.getElementById('countryflag1').src = "https://flagcdn.com/16x12/" + flag + ".png";
+    var flag = code.value.substr(0, code.value.length - 1).toLowerCase();
+    document.getElementById('countryflag1').src = "https://flagcdn.com/16x12/" + flag + ".png";
 }
 
 function setcountryflag2(code) {
-  var flag = code.value.substr(0, code.value.length - 1).toLowerCase();
-  document.getElementById('countryflag2').src = "https://flagcdn.com/16x12/" + flag + ".png";
+    var flag = code.value.substr(0, code.value.length - 1).toLowerCase();
+    document.getElementById('countryflag2').src = "https://flagcdn.com/16x12/" + flag + ".png";
 }
 
 function convert(currency1, currency2, value) {
-  const host = "api.frankfurter.app";
-  fetch(
-    `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
-  )
-    .then((val) => val.json())
-    .then((val) => {
-      ans.value = Object.values(val.rates)[0];
-    });
+    const host = "api.frankfurter.app";
+    fetch(
+            `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
+        )
+        .then((val) => val.json())
+        .then((val) => {
+            ans.value = Object.values(val.rates)[0];
+        });
 }
+
+// right click 
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault()
+}, false)
+
+//loader
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector('.loader').style.display = 'none';
+        document.getElementById('app').style.display = 'block';
+    }, 5000);
+});
